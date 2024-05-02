3592514b9cc2b8834b501a333ab75b7270e211e6
Guhxs1010 <gustavogithub1@gmail.com>
2024-05-02 16:22:21 -0300

    Initial commit

---
+++ routes.js
@@ -0,0 +1,30 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import Bem_Vindo from './pages/index';
+import Acesso from './pages/entrada';
+import TelaDeSucesso from './pages/Telasucesso';
+import Cadastro from './pages/Cadastro';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <Stack.Navigator initialRouteName="index" screenOptions={{ headerShown: false }}>
+            <Stack.Screen
+                name="index"
+                component={Bem_Vindo} />
+
+            <Stack.Screen
+                name="entrada"
+                component={Acesso} />
+
+            <Stack.Screen
+                name="TelaDeSucesso"
+                component={TelaDeSucesso} />
+
+            <Stack.Screen 
+            name="Cadastro" 
+            component={Cadastro} />
+
+        </Stack.Navigator>
+    );
+}
